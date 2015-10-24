aa9754bcefc85186343db1a03ee2379cf45925f8
Paul Wang <p.wang8@outlook.com>
2015-10-24 16:36:24 -0600

    Auto generated files

--- src/LogFileReader.cpp
+++ src/LogFileReader.cpp
@@ -2,6 +2,7 @@
 #include <QFile>
 #include <QString>
 #include <QTextStream>
+#include <QTime>
 
 #include "LogFileReader.h"
 
@@ -47,12 +48,44 @@ bool LogFileReader::readAll(const QString& fileName)
 // File input is a csv file in the format of hh:mm:ss:zzz, voltage, current
 bool LogFileReader::parseLine(const QString& line, BatteryData& batteryData) const
 {
-    // TODO implement this first
+    unsigned int allowedCharLength = 14;
+    std::string allowedChars [allowedCharLength] = {"1","2","3","4","5","6","7","8","9","0",":",",",".","-"};
+    bool tempBool;
+    for(unsigned int count = 0; count < line.toStdString().length(); count++)
+    {
+        tempBool = false;
+        for(unsigned int count1 = 0; count1 < allowedCharLength; count1++)
+        {
+            if(allowedChars[count1] == line.toStdString().substr(count,1))
+            {
+                tempBool = true;
+            }
+        }
+        if(!tempBool)
+        {
+            return false;
+        }
+    }
 
-    // This is here to the compiler happy. Otherwise the compile
-    // will have an error warning about an unused variable. Remove this
-    // when you use it.
-    Q_UNUSED(line);
-    Q_UNUSED(batteryData);
+    //example input= 07:01:07.024, 3.538654, 25.523571
+    QStringList lineSplit = line.split(",");
+    if(lineSplit.length()!= 3)
+        return false;
+    QStringList timeSplit = lineSplit[0].split(":");
+    if(timeSplit.length()!=3)
+        return false;
+    QStringList secondSplit = timeSplit[2].split(".");
+    if(secondSplit.length()!=2)
+        return false;
+
+    QTime time = QTime(timeSplit[0].toInt(),    //hour
+                        timeSplit[1].toInt(),   //minute
+                        secondSplit[0].toInt(), //second
+                        secondSplit[1].toInt());//millisec
+
+    batteryData = BatteryData(time,
+                              lineSplit[1].toDouble(),//voltage
+                              lineSplit[2].toDouble());//current
+
     return true;
 }
