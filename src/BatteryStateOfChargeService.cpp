d222b4e1d7109c9d533667b4f90c5b59343d6900
Paul Wang <p.wang8@outlook.com>
2015-11-21 12:01:20 -0700

    changed precision bug of time not calculated properly

--- src/BatteryStateOfChargeService.cpp
+++ src/BatteryStateOfChargeService.cpp
@@ -1,8 +1,10 @@
 #include "BatteryStateOfChargeService.h"
+#include <stdio.h>
 
 namespace
 {
     const double BATTERY_AMP_HOUR_CAPACITY = 123.0;
+    double ampHourTotal = 0;
 }
 
 BatteryStateOfChargeService::BatteryStateOfChargeService(double initialStateOfChargePercent)
@@ -16,12 +18,32 @@ BatteryStateOfChargeService::~BatteryStateOfChargeService()
 
 double BatteryStateOfChargeService::totalAmpHoursUsed() const
 {
-    return 0.0;
+    double diffOfHours = myTime.hour() - prevTime->hour();
+    double diffOfMinutes = myTime.minute() - prevTime->minute();
+    double diffOfSeconds = myTime.second() - prevTime->second();
+    double diffOfMiliSec = myTime.msec() - prevTime->msec();
+    double diffOfTime = diffOfSeconds + (diffOfMiliSec/1000);
+    diffOfTime = diffOfMinutes + (diffOfSeconds/60);
+    diffOfTime = diffOfHours + (diffOfMinutes/60);
+
+    printf("diffInTime:%lf\n",diffOfTime);
+    //printf("hours:%lf,minutes:%lf,seconds:%lf,millisec:%lf,time:%lf\n",diffOfHours,diffOfMinutes,diffOfSeconds,diffOfMiliSec,diffOfTime);
+    //printf("curr hour:%i,minute:%i,second:%i,millis:%i\nprev hour:%i,minute:%i,second:%i,millis:%i\n",
+      //     myTime.hour(),myTime.minute(),myTime.second(),myTime.msec(),prevTime->hour(),prevTime->minute(),prevTime->second(),prevTime->msec());
+
+
+    double changeInAmpHour = myCurrent * diffOfTime;
+    ampHourTotal += changeInAmpHour;
+    return ampHourTotal;
 }
 
 bool BatteryStateOfChargeService::isCharging() const
 {
-    return false;
+    if(myCurrent>=0)
+    {
+        return false;
+    }
+    return true;
 }
 
 QTime BatteryStateOfChargeService::timeWhenChargedOrDepleted() const
@@ -31,6 +53,8 @@ QTime BatteryStateOfChargeService::timeWhenChargedOrDepleted() const
 
 void BatteryStateOfChargeService::addData(const BatteryData& batteryData)
 {
-    Q_UNUSED(batteryData);
-    // Update your variables here.
+    myVoltage = batteryData.voltage;
+    myCurrent = batteryData.current;
+    prevTime = new QTime(myTime.hour(),myTime.minute(),myTime.second(),myTime.msec());
+    myTime = batteryData.time;
 }
