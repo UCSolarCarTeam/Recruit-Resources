a499344a25d5be2397e726e22134129925372669
Baba <baba.adeniran@gmail.com>
2015-11-21 13:49:20 -0700

    Fixed error checking batDataFailure.csv file

---
+++ UofCSolarRecruitTraining/src/BatteryStateOfChargeService.cpp
@@ -0,0 +1,79 @@
+#include "BatteryStateOfChargeService.h"
+#include <QString>
+#include <QTextStream>
+#include "LogFileReader.h"
+
+namespace
+{
+    const double BATTERY_AMP_HOUR_CAPACITY = 123.0;
+    const double MILLISECONDS_TO_HOURS = 2.77778e-7;
+}
+
+BatteryStateOfChargeService::BatteryStateOfChargeService(double initialStateOfChargePercent)
+: initialStateOfChargePercent_(initialStateOfChargePercent), newCurrent(0)
+{
+    ampHours = BATTERY_AMP_HOUR_CAPACITY * (initialStateOfChargePercent_/100);
+}
+
+BatteryStateOfChargeService::~BatteryStateOfChargeService()
+{
+}
+
+double BatteryStateOfChargeService::totalAmpHoursUsed() const
+{
+    return BATTERY_AMP_HOUR_CAPACITY-ampHours;
+}
+
+bool BatteryStateOfChargeService::isCharging() const
+{
+    if (newCurrent<0)
+        return false;
+    else
+        return true;
+}
+
+QTime BatteryStateOfChargeService::timeWhenChargedOrDepleted() const
+{
+    double timeLeft;
+
+
+    timeLeft = (BATTERY_AMP_HOUR_CAPACITY-ampHours)/newCurrent;
+    timeLeft = timeLeft/MILLISECONDS_TO_HOURS;
+    timeLeft = qAbs(timeLeft);
+
+     QTime baseTime (0, 0, 0, 0);
+     QTime timeLeftTillDepletionorCharge;
+     timeLeftTillDepletionorCharge = baseTime.addMSecs(timeLeft);
+
+    return timeLeftTillDepletionorCharge;
+}
+
+void BatteryStateOfChargeService::addData(const BatteryData& batteryData)
+{
+
+
+
+    double timeChange;
+    double changeInAmpHours;
+    QTime previousTime;
+
+
+    previousCurrent = newCurrent;
+    newCurrent = batteryData.current;
+     if (previousCurrent == 0) //For the very first line of data
+         averageCurrent = newCurrent;
+     else
+        averageCurrent = (previousCurrent+newCurrent)/2;
+
+
+    previousTime = newTime;
+    newTime = batteryData.time;
+    timeChange =previousTime.msecsTo(newTime);
+    timeChange = timeChange * MILLISECONDS_TO_HOURS ;
+
+    changeInAmpHours = averageCurrent * timeChange;
+    ampHours +=  changeInAmpHours;
+
+
+}
+
---
+++ UofCSolarRecruitTraining/src/LogFileReader.cpp
@@ -0,0 +1,83 @@
+#include <QDebug>
+#include <QFile>
+#include <QString>
+#include <QTextStream>
+#include <QStringList>
+#include "LogFileReader.h"
+
+namespace
+{
+const QString TIME_FORMAT = "hh:mm:ss.zzz";
+const int NUMBER_OF_COLUMNS = 3;
+}
+
+LogFileReader::LogFileReader()
+{
+}
+
+LogFileReader::~LogFileReader()
+{
+}
+
+bool LogFileReader::readAll(const QString& fileName)
+{
+    QFile file(fileName);
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "Unable to open file" << fileName;
+        return false;
+    }
+
+    QTextStream input(&file);
+    while(!input.atEnd())
+    {
+        QString line = input.readLine();
+        BatteryData batteryData;
+        if (!parseLine(line, batteryData))
+        {
+            qDebug() << "Error while parsing" << line;
+        }
+        else
+        {
+        // This is how to send out a signal in QT using the emit keyword.
+        // This line notifies how any classes listening to this signal
+        // that battery data has been received.
+        emit batteryDataReceived(batteryData);
+        }
+    }
+
+    return true;
+}
+
+// File input is a csv file in the format of hh:mm:ss:zzz, voltage, current
+bool LogFileReader::parseLine(const QString& line, BatteryData& batteryData) const
+{
+    // TODO implement this first
+    QStringList sections = line.split(", ");
+    if(sections.length() != NUMBER_OF_COLUMNS)
+    {
+     return false;
+    }
+
+
+    QString stringTime = sections.at(0);
+    batteryData.time = QTime::fromString (stringTime, TIME_FORMAT);
+
+    bool voltageOkay;
+    bool currentOkay;
+
+    batteryData.voltage = sections.at(1).toDouble(&voltageOkay);
+    batteryData.current = sections.at(2).toDouble(&currentOkay);
+
+    if(!voltageOkay || !currentOkay || !batteryData.time.isValid())
+    {
+     return false;
+    }
+    else
+    {
+     return true;
+    }
+
+
+
+}
