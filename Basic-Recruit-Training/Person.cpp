02ceac143afe6369af2d8493520f4195b143be8c
tehseen i <tehseen i>
2017-01-14 15:11:42 -0700

    I addressed the errors in my code

--- Basic-Recruit-Training/Person.cpp
+++ Basic-Recruit-Training/Person.cpp
@@ -1,38 +1,45 @@
 #include "Person.h"
 #include <iostream>
 #include <string>
+
 Person::Person()
 {
     age_ = new int(0);
     name_="";
 }
+
 Person::Person(const std::string& name, int age)
 {
     age_ = new int(age);
     name_ = name;
-
 }
+
 Person::~Person()
 {
     delete age_;
 }
+
 int Person::getAge()const
 {
     return *age_;
 }
+
 void Person::printInfo()
 {
     std::cout << getName() << std::endl;
     std::cout << getAge() << std::endl;
-
 }
+
 void Person::setName(const std::string& newName)
 {
     name_ = newName;
 }
-void Person::setAge(int newAge){
+
+void Person::setAge(int newAge)
+{
     *age_ = newAge;
 }
+
 int Person::combinedAge(Person** x, int size)
 {
     int value = 0;
@@ -42,6 +49,7 @@ int Person::combinedAge(Person** x, int size)
     std::cout << value;
     return value;
 }
+
 void Person::birthday(Person& x)
 {
     x.setAge(x.getAge()+1);
