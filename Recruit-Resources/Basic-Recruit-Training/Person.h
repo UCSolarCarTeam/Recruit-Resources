1c64b744a226372771e1446dd64a64b9cf520782
Josie Khampheng <jtdkhamp@gmail.com>
2017-11-04 11:55:53 -0600

    added my name

---
+++ Recruit-Resources/Basic-Recruit-Training/Person.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+
+class Person
+{
+public:
+    Person();
+    ~Person();
+    Person(const std::string& name, int age);
+
+    // setters and getters
+    int getAge() const;
+    const std::string& getName() const;
+    void setName(const std::string& newName);
+    void setAge(int newAge);
+
+    // Print out the name and age of the person
+    void printInfo() const;
+    // Print the combined age of everyone pointed to by personArray
+    static int combinedAge(Person** personArray, int size);
+    // Increases the Person's Age by 1
+    static void birthday(Person& x);
+
+private:
+    std::string name_;
+    int* age_; // age_ should be dynamically allocated
+
+};
+
+
