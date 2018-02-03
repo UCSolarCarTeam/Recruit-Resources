945c46a0a3b11cee254469d1ae1e1cbac97eb009
Zelin Zhou <kobezhou1998@gmail.com>
2018-02-03 14:12:42 -0700

    Fix some format issues,need to check my calculation error

--- Advanced-Recruit-Training/src/BatteryStateDisplayService.cpp
+++ Advanced-Recruit-Training/src/BatteryStateDisplayService.cpp
@@ -9,13 +9,10 @@ BatteryStateDisplayService::BatteryStateDisplayService(
     const I_BatteryDataSource& batteryDataSource,
     I_BatteryStateOfChargeService& batteryStateOfChargeService)
 : batteryStateOfChargeService_(batteryStateOfChargeService)
-{
-    // This function is what "connects" the signal to the slot. So whenever the
-    // signals it emitted, the slot will be called and the signal arguements
-    // will be passed into the slot.
-    connect(&batteryDataSource, SIGNAL(batteryDataReceived(const BatteryData&)),
+    {
+        connect(&batteryDataSource, SIGNAL(batteryDataReceived(const BatteryData&)),
         this, SLOT(handleBatteryDataReceived(const BatteryData&)));
-}
+    }
 
 BatteryStateDisplayService::~BatteryStateDisplayService()
 {
@@ -24,7 +21,6 @@ BatteryStateDisplayService::~BatteryStateDisplayService()
 void BatteryStateDisplayService::handleBatteryDataReceived(const BatteryData& batteryData)
 {
     batteryStateOfChargeService_.addData(batteryData);
-
     QTextStream(stdout) << "Voltage: " << batteryData.voltage
         << " Current: " << batteryData.current
         << " Total Ah used: " << batteryStateOfChargeService_.totalAmpHoursUsed();
@@ -36,9 +32,4 @@ void BatteryStateDisplayService::handleBatteryDataReceived(const BatteryData& ba
         else{
           QTextStream(stdout)  <<" Time until depletion:" <<batteryStateOfChargeService_.timeWhenChargedOrDepleted().toString("hh:mm:ss.zzz") << "\n";
         }
-
 }
-
-
-    // TODO: Print out time until the battery is fully charged or depleted.
-
--- Advanced-Recruit-Training/src/BatteryStateOfChargeService.cpp
+++ Advanced-Recruit-Training/src/BatteryStateOfChargeService.cpp
@@ -1,8 +1,5 @@
 #include "BatteryData.h"
 #include "BatteryStateOfChargeService.h"
-
-
-
 namespace
 {
     const double BATTERY_AMP_HOUR_CAPACITY = 123.0;
@@ -13,8 +10,8 @@ BatteryStateOfChargeService::BatteryStateOfChargeService(double initialStateOfCh
 , amphoursUsed(0)
 , previousCurrent(0)
 , averageCurrent(0)
-,current(0)
-,check1(false)
+, current(0)
+, check1(false)
 
 {
 }
@@ -25,71 +22,57 @@ BatteryStateOfChargeService::~BatteryStateOfChargeService()
 
 double BatteryStateOfChargeService::totalAmpHoursUsed() const
 {
-
-
      return amphoursUsed;
 }
 
 bool BatteryStateOfChargeService::isCharging() const
-{
-if(current<0)
-{
+    {
+      if(current < 0)
+    {
     return true;
-}
-else
-{return false;}
-
+    }
+    else
+    {
+        return false;
+    }
 }
 
 QTime BatteryStateOfChargeService::timeWhenChargedOrDepleted() const
-{
-
+ {
     double timeUntilCharged;
     double timeUntilDepletion;
 
-   QTime charged  (0,0,0,0);
-   timeUntilCharged= (amphoursUsed / current)*(3600 * 1000);
+    QTime charged  (0,0,0,0);
+    timeUntilCharged= (amphoursUsed / current)*(3600 * 1000);
 
-   charged = charged.addMSecs(timeUntilCharged);
-   QTime depletion (0,0,0,0);
-   timeUntilDepletion=((BATTERY_AMP_HOUR_CAPACITY-amphoursUsed)*3600000)/current;
-   depletion = depletion.addMSecs(qAbs(timeUntilDepletion));
+    charged = charged.addMSecs(timeUntilCharged);
+    QTime depletion (0,0,0,0);
+    timeUntilDepletion=((BATTERY_AMP_HOUR_CAPACITY-amphoursUsed)*3600000)/current;
+    depletion = depletion.addMSecs(qAbs(timeUntilDepletion));
 
    if (isCharging()==true){
         return charged;
     }else{
         return depletion;
     }
-
 }
 
 void BatteryStateOfChargeService::addData(const BatteryData& batteryData)
      {
-     double initialAmountOfAmphours = (initialStateOfChargePercent_/100) * BATTERY_AMP_HOUR_CAPACITY;
-     current = batteryData.current;
+        double initialAmountOfAmphours = (initialStateOfChargePercent_/100) * BATTERY_AMP_HOUR_CAPACITY;
+        current = batteryData.current;
      if(check1)
      {
-    averageCurrent = (batteryData.current+previousCurrent)/2;
-
-    double timeIntervel = (double)previousTime.msecsTo(batteryData.time);
-    double hours = timeIntervel/3600000;
-    amphoursUsed += averageCurrent * hours;
-
-
-
+        averageCurrent = (batteryData.current+previousCurrent)/2;
+        double timeIntervel = (double)previousTime.msecsTo(batteryData.time);
+        double hours = timeIntervel/3600000;
+        amphoursUsed += averageCurrent * hours;
     }
-    else {
+    else
+     {
          amphoursUsed = initialAmountOfAmphours;
-
-    }
+     }
 check1 =true;
       previousTime=batteryData.time;
-previousCurrent= batteryData.current;
-
-
-
-
-    // This is where you can update your variables
-    // Hint: There are many different ways that the totalAmpHoursUsed can be updated
-    // i.e: Taking a running average of your data values, using most recent data points, etc.
-}
+      previousCurrent= batteryData.current;
+    }
