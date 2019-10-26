c0db69d064dac35b584637b7478ca160c68a057b
JaydenBischoff <jayden.bischoff1@ucalgary.ca>
2019-10-26 14:18:50 -0600

    Added to battery component and app component

--- Recruit-Training/Telemetry-Recruit-Training/src/app/app-routing.module.ts
+++ Recruit-Training/Telemetry-Recruit-Training/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BatteryComponent } from './tabs/Battery/battery.component';
+
+const routes: Routes = [
+    { path: 'battery', component: BatteryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Recruit-Training/Telemetry-Recruit-Training/src/app/app.component.ts
+++ Recruit-Training/Telemetry-Recruit-Training/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Telemetry-Recruit-Training';
+  routeLinks: any[];
+  activeLinkIndex = -1;
+
+  constructor(
+    private router: Router
+  ) {
+    this.routeLinks = [
+      {
+        label: 'Battery',
+        link: './battery',
+        index: 0
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.routeLinks.indexOf(
+        this.routeLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
+
+  getActiveClass(indexofRouteLink) {
+    let tabsclass = 'mat-tab-link';
+    if (this.activeLinkIndex === indexofRouteLink) {
+      tabsclass = 'mat-tab-link mat-tab-label-active';
+    }
+    return tabsclass;
+  }
+
 }
