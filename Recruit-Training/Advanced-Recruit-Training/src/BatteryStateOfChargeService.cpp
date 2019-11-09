7bcd591c1de2dc443646de2f55aab0920f275b63
JensVarughse <jens.varughese1@ucalgary.ca>
2019-11-09 09:28:30 -0800

    Implement functions of BatteryStateOfChargeService
    Implement isCharging to return true if current is negative
    Implement timeWhenChargedOrDepleted to calculate the total hours, then convert to hms format
    Implement addData to update current, time, and calculate ampHours
    Implement totalAmpHoursUsed to return ampHours(ampHours was not calculated here as it is a read only function)

--- Recruit-Training/Advanced-Recruit-Training/src/BatteryStateOfChargeService.cpp
+++ Recruit-Training/Advanced-Recruit-Training/src/BatteryStateOfChargeService.cpp
@@ -1,12 +1,13 @@
 #include "BatteryStateOfChargeService.h"
+#include "BatteryData.h"
 
 namespace
 {
-    const double BATTERY_AMP_HOUR_CAPACITY = 123.0;
+const double BATTERY_AMP_HOUR_CAPACITY = 123.0;
 }
 
 BatteryStateOfChargeService::BatteryStateOfChargeService(double initialStateOfChargePercent)
-: initialStateOfChargePercent_(initialStateOfChargePercent)
+    : initialStateOfChargePercent_(initialStateOfChargePercent)
 {
 }
 
@@ -16,17 +17,41 @@ BatteryStateOfChargeService::~BatteryStateOfChargeService()
 
 double BatteryStateOfChargeService::totalAmpHoursUsed() const
 {
-    return 0.0;
+    return ampHours_;
 }
 
 bool BatteryStateOfChargeService::isCharging() const
 {
+    if(current_ < 0)
+        return true;
     return false;
 }
 
 QTime BatteryStateOfChargeService::timeWhenChargedOrDepleted() const
 {
-    return QTime::currentTime();
+    QTime time;
+    double totalHours;
+    int hours;
+    int remainingHours=0;
+    int minutes;
+    int seconds;
+
+    if(isCharging())
+        totalHours = -1 * totalAmpHoursUsed() / current_;
+    else
+        totalHours = (BATTERY_AMP_HOUR_CAPACITY-totalAmpHoursUsed()) / current_;
+
+    hours = (int)totalHours;
+    minutes = (int)((totalHours - hours) * 60);
+    seconds = (int)((totalHours*60 - hours*60 - minutes) * 60);
+
+    //uses msec as a place holder for the remaining hours as the hours in QTime could only hold up to 24
+    remainingHours = ((int)(totalHours/24)) * 24;
+    hours -= remainingHours;
+
+    time.setHMS(hours, minutes, seconds, remainingHours);
+
+    return time;
 }
 
 void BatteryStateOfChargeService::addData(const BatteryData& batteryData)
@@ -35,4 +60,12 @@ void BatteryStateOfChargeService::addData(const BatteryData& batteryData)
     // This is where you can update your variables
     // Hint: There are many different ways that the totalAmpHoursUsed can be updated
     // i.e: Taking a running average of your data values, using most recent data points, etc.
+    current_ = batteryData.current;
+    timeOld_ = timeNew_;
+    timeNew_ = batteryData.time;
+
+    if(timeOld_.isNull()) //checks if timeOld_ has no a value (first itteration)
+        ampHours_ += BATTERY_AMP_HOUR_CAPACITY * initialStateOfChargePercent_/100;
+    else
+        ampHours_ += (current_ * timeOld_.secsTo(timeNew_)/3600);
 }
