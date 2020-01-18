19addf7eec5c0051b91ff8c535ddf019cd972fb1
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2020-01-18 14:55:20 -0700

    Fixed the letter s

--- Recruit-Training/Advanced-Recruit-Training/src/BatteryStateDisplayService.cpp
+++ Recruit-Training/Advanced-Recruit-Training/src/BatteryStateDisplayService.cpp
@@ -39,7 +39,7 @@ void BatteryStateDisplayService::handleBatteryDataReceived(const BatteryData& ba
 
      else
 
-     s{
+     {
          QTextStream(stdout) << "Time until depleted:" << batteryStateOfChargeService_.
                                                           timeWhenChargedOrDepleted().toString(Qt::ISODateWithMs) << endl;
      }
