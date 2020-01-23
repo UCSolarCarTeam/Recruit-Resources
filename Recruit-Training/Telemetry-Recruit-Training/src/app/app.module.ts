6299ed2c887094ca8cb6c268c5e5dfbf7439012f
JensVarughse <jens.varughese1@ucalgary.ca>
2020-01-22 18:39:16 -0800

    Add Mppt Component in app.module.ts

--- Recruit-Training/Telemetry-Recruit-Training/src/app/app.module.ts
+++ Recruit-Training/Telemetry-Recruit-Training/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BatteryComponent } from './tabs/battery/battery.component';
+import { MpptComponent } from './tabs/mppt/mppt.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BatteryComponent,
+    MpptComponent,
   ],
   imports: [
     BrowserModule,
