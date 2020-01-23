6824471545571feebedd7879f6014663e41672d7
JensVarughse <jens.varughese1@ucalgary.ca>
2020-01-22 18:41:36 -0800

    Add mppt route in app-routing.module.ts

--- Recruit-Training/Telemetry-Recruit-Training/src/app/app-routing.module.ts
+++ Recruit-Training/Telemetry-Recruit-Training/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BatteryComponent } from './tabs/battery/battery.component';
+import { MpptComponent } from './tabs/mppt/mppt.component';
 
 const routes: Routes = [
     { path: 'battery', component: BatteryComponent },
+    { path: 'mppt', component: MpptComponent },
     { path: '**', redirectTo: '/battery', pathMatch: 'full' }
 ];
 
