f2018e34bc8539dd0a0712052528b33fb124a118
Rahat <islamrahat735@gmail.com>
2020-01-22 19:34:04 -0700

    TO BE SQUASHED

--- Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
+++ Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
@@ -11,36 +11,47 @@ using namespace std;
 
 void foo(int *a, int b);
 void bar(int *a, int b);
+void Myswap(int *a, int *b);
 
 int main()
 {
     int x = 3;
     int y = 4;
     int *p = &x;
+    int *q = &y;
 
     //TODO in the line below predict what what is going to be output
-    cout << "Predicted value of p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of p: " /* << address of x*/ << endl;
     cout << "Actual value of p: " << p << endl;
-    cout << "Predicted value &x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value &x: " /* << address of x*/ << endl;
     cout << "Actual value &x: " << &x << endl;
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " /* << 3*/ << endl;
     cout << "Actual value of *p: " << *p << endl;
     
     foo(p, x);
     
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " /* << 42*/ << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of x: " /* << PLACE YOUR PREDICTION HERE: 42*/ << endl;
     cout << "Actual value of x: " << x << endl;
     foo(p, y);
 
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE:42*/ << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of y: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of y: " /* << PLACE YOUR PREDICTION HERE:4*/ << endl;
     cout << "Actual value of y: " << y << endl;
 
-
+cout<< " value of x before swap: " << x << endl;
+cout<< " value of y before swap: " << y << endl;
+cout << " value of *p before swap: " << *p << endl;
+   cout << " value of *q before swap: " << *q << endl;
+    
     //start writing MySwap here
+   Myswap(p, q);
+   cout<< " value of x after swap: " << x << endl;
+cout<< " value of y after swap: " << y << endl;
+   cout << "Actual value of *p after swap: " << *p << endl;
+   cout << "Actual value of *q after swap: " << *q << endl;
     
     return 0;
 }
@@ -56,3 +67,12 @@ void var(int *a, int b)
     *a = 365;
     b = 912;
 }
+ void Myswap (int *a, int *b)
+ {
+int temp1;
+int temp2;
+temp1= *b;
+temp2= *a;
+*b=temp2;
+*a=temp1;
+ }
