05298be005b4b2860de840b976292abcb297a793
JaydenBischoff <jayden.bischoff1@ucalgary.ca>
2020-11-05 16:45:12 -0700

    Created the basic angular training for the Telemetry teaser training

---
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+
+  public input: any;
+  title = 'Telemetry Recruit Training';
+  weather = 50;
+  speed = 0;
+  battery = 0;
+
+  onMouseWeather(event: MouseEvent) {
+    this.weather = parseInt((event.target as HTMLInputElement).value);
+  }
+
+  onKeySpeed(event: KeyboardEvent) {
+    this.speed = parseInt((event.target as HTMLInputElement).value);
+  }
+
+  onKeyBattery(event: KeyboardEvent) {
+    this.battery = parseInt((event.target as HTMLInputElement).value);
+  }
+
+  calculateRange() {
+
+  }
+}
