8139e791b05bd6897ad213c2ef8e783bb0ad0e9b
Dillonsd <dillonapple7h@gmail.com>
2020-11-08 00:33:05 +0400

    Embedded Teaser Complete

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -1,6 +1,42 @@
 #include "Training.h"
 
 void trainingTask(uint8_t* data)
-{
+{	
+	validData = 0;
+	uint8_t motor1Data = data[0];
+	uint8_t motor2Data = data[1];
+	int forward1 = motor1Data >> 7;						// Select bit 7 of motor 1
+	int forward2 = motor2Data >> 7;						// Select bit 7 of motor 2
+	int speed1 = (motor1Data & 0b00111110) >> 1; 		// Speed of motor 1
+	int speed2 = (motor2Data & 0b00111110) >> 1; 		// Speed of motor 2
+	int motor1On = motor1Data & 1;						// Motor 1 on
+	int motor2On = motor2Data & 1;						// Motor 2 on
+	int sign1 = (motor1Data >> 6) & 1;					// Direction of velocity of motor1
+	int sign2 = (motor2Data >> 6) & 1;					// Direction of velocity of motor2
+	if((motor1On == motor2On) &&
+		(motor2On == 0)){								// If both motors off
+		if((speed1 == speed2) &&
+			(speed2 == 0))								// and speed is 0 it's valid
+			validData += 3;
+	} else if(motor1On == motor2On){					// If both motors on
+		if(forward1 == forward2) {						// If motors going in the same direction
+			if(forward1 == !sign1)						// If motor1 is going in same direction as velocity
+				validData += 1;							// it's valid
 
+			if(forward2 == !sign2) 	// If motor2 is going in same direction as velocity
+				validData += 2;							// it's valid
+		}
+	}
+
+	uint8_t lightData = data[2];
+	int headlights = lightData & 0x07;								// Select bits that represent headlights
+	if(headlights == 1 || headlights == 2 || headlights == 4) {		// If headlights are in more than one state it's invalid
+		if((lightData >> 5) & 1) {			// If hazards are on
+			if((lightData >> 3) & 1 && 		// and both signals are on
+				(lightData >> 4) & 1)
+				validData += 4;				// it's valid
+		} else if(((lightData >> 3) & 1) + 	// If hazards are off and only one or neither signal is on
+			((lightData >> 4) & 1) <= 1)
+			validData += 4;					// it's valid
+	}
 }
--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -5,24 +5,44 @@
 void runTrainingTests()
 {
     // Run all the tests here using RUN_TEST()
+    RUN_TEST(test_EverythingValid);
+    RUN_TEST(test_EverythingInvalid);
+    RUN_TEST(test_OnlyLightsInvalid);
+    RUN_TEST(test_OnlyMotorsInvalid);
 }
 
 void test_EverythingValid()
 {
-
+	uint8_t allValidData[] = {  0b10111111,		// Motor on, going forward with positive velocity
+								0b10111111,		// Motor on, going forward with positive velocity
+								0b01111100 };	// High headlights, with hazards on, brakes off
+	trainingTask(allValidData);
+	TEST_ASSERT_EQUAL_INT8(validData, 7);		// Expecting 7 for all valid
 }
 
 void test_EverythingInvalid()
 {
-
+	uint8_t allValidData[] = {  0b00111111,		// Motor on, going reverse with positive velocity
+								0b11111111,		// Motor on, going forward with negative velocity
+								0b01100111 };	// Wrong headlights, with hazards on, signals off, brakes off
+	trainingTask(allValidData);
+	TEST_ASSERT_EQUAL_INT8(validData, 0);		// Expecting 0 for all invalid
 }
 
 void test_OnlyLightsInvalid()
 {
-
+	uint8_t allValidData[] = {  0b00000000,		// Motor off
+								0b00000000,		// Motor off
+								0b01100100 };	// High headlights, with hazards on, signals off, brakes off
+	trainingTask(allValidData);
+	TEST_ASSERT_EQUAL_INT8(validData, 3);		// Expecting 3 for motors
 }
 
 void test_OnlyMotorsInvalid()
 {
-
+	uint8_t allValidData[] = {  0b00000110,		// Motor off, non-zero speed
+								0b00010110,		// Motor off, non-zero speed
+								0b01010010 };	// Low headlights, with hazards off, one signal on, brakes off
+	trainingTask(allValidData);
+	TEST_ASSERT_EQUAL_INT8(validData, 4);		// Expecting 4 for lights
 }
