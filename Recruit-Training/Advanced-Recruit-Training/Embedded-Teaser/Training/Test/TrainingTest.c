c3d2b63d7fb8565f05a6cac30ab1d618d9f0e65d
Dillonsd <dillonapple7h@gmail.com>
2020-11-21 23:33:45 +0400

    Unit test messages added

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -17,10 +17,10 @@ void test_EverythingValid()
 								0b10111111,		// Motor on, going forward with positive velocity
 								0b01111100 };	// High headlights, with hazards on, brakes off
 	trainingTask(allValidData);
-	TEST_ASSERT_EQUAL_INT8(validData, 7);		// Expecting 7 for all valid
-	TEST_ASSERT_EQUAL_INT8(outputArray[0], 1);	// Expecting 1 for motor 1
-	TEST_ASSERT_EQUAL_INT8(outputArray[1], 1);	// Expecting 1 for motor 2
-	TEST_ASSERT_EQUAL_INT8(outputArray[2], 1);	// Expecting 1 for lights
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(validData, 7, "Function did not set validData to 0b111 for all valid");				// Expecting 7 for all valid
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[0], 1, "Function did not set outputArray[0] to 1 for valid motor 1");	// Expecting 1 for motor 1
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[1], 1, "Function did not set outputArray[1] to 1 for valid motor 2");	// Expecting 1 for motor 2
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[2], 1, "Function did not set outputArray[2] to 1 for valid lights");		// Expecting 1 for lights
 }
 
 void test_EverythingInvalid()
@@ -29,10 +29,10 @@ void test_EverythingInvalid()
 								0b11111111,		// Motor on, going forward with negative velocity
 								0b01100111 };	// Wrong headlights, with hazards on, signals off, brakes off
 	trainingTask(allValidData);
-	TEST_ASSERT_EQUAL_INT8(validData, 0);		// Expecting 0 for all invalid
-	TEST_ASSERT_EQUAL_INT8(outputArray[0], 0);	// Expecting 0 for motor 1
-	TEST_ASSERT_EQUAL_INT8(outputArray[1], 0);	// Expecting 0 for motor 2
-	TEST_ASSERT_EQUAL_INT8(outputArray[2], 0);	// Expecting 0 for lights
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(validData, 0, "Function did not set validData to 0b000 for all invalid");			// Expecting 0 for all invalid
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[0], 0, "Function did not set outputArray[0] to 0 for invalid motor 1");	// Expecting 0 for motor 1
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[1], 0, "Function did not set outputArray[1] to 0 for invalid motor 2");	// Expecting 0 for motor 2
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[2], 0, "Function did not set outputArray[2] to 0 for invalid lights");	// Expecting 0 for lights
 }
 
 void test_OnlyLightsInvalid()
@@ -41,10 +41,10 @@ void test_OnlyLightsInvalid()
 								0b00000000,		// Motor off
 								0b01100100 };	// High headlights, with hazards on, signals off, brakes off
 	trainingTask(allValidData);
-	TEST_ASSERT_EQUAL_INT8(validData, 3);		// Expecting 3 for motors
-	TEST_ASSERT_EQUAL_INT8(outputArray[0], 1);	// Expecting 1 for motor 1
-	TEST_ASSERT_EQUAL_INT8(outputArray[1], 1);	// Expecting 1 for motor 2
-	TEST_ASSERT_EQUAL_INT8(outputArray[2], 0);	// Expecting 0 for lights
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(validData, 3, "Function did not set validData to 0b011 for invalid lights and valid motors");	// Expecting 3 for motors
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[0], 1, "Function did not set outputArray[0] to 1 for valid motor 1");				// Expecting 1 for motor 1
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[1], 1, "Function did not set outputArray[1] to 1 for valid motor 2");				// Expecting 1 for motor 2
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[2], 0, "Function did not set outputArray[2] to 0 for invalid lights");				// Expecting 0 for lights
 }
 
 void test_OnlyMotorsInvalid()
@@ -53,8 +53,8 @@ void test_OnlyMotorsInvalid()
 								0b00011110,		// Motor off, non-zero speed
 								0b01010010 };	// Low headlights, with hazards off, one signal on, brakes off
 	trainingTask(allValidData);
-	TEST_ASSERT_EQUAL_INT8(validData, 4);		// Expecting 4 for lights
-	TEST_ASSERT_EQUAL_INT8(outputArray[0], 0);	// Expecting 0 for motor 1
-	TEST_ASSERT_EQUAL_INT8(outputArray[1], 0);	// Expecting 0 for motor 2
-	TEST_ASSERT_EQUAL_INT8(outputArray[2], 1);	// Expecting 1 for lights
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(validData, 4, "Function did not set validData to 0b100 for valid lights and invalid motors");	// Expecting 4 for lights
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[0], 0, "Function did not set outputArray[0] to 0 for valid motor 1");				// Expecting 0 for motor 1
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[1], 0, "Function did not set outputArray[1] to 0 for valid motor 2");				// Expecting 0 for motor 2
+	TEST_ASSERT_EQUAL_INT8_MESSAGE(outputArray[2], 1, "Function did not set outputArray[2] to 1 for valid lights");					// Expecting 1 for lights
 }
