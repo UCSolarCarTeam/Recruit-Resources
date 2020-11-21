da72f5dbf588d24f6433ada7f861b8d846ba2b94
Ahmed_Abdullah <ahmedi3aaa@hotmail.com>
2020-11-21 02:55:00 +0200

    Done with requested fixes

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -1,34 +1,43 @@
 #include "Training.h"
 
-void trainingTask(uint8_t* data)
+void trainingTask (uint8_t* data)
 {
 	validData = 0;
+	int motorOneState = motorCheck (*data);
+	int motorTwoState = motorCheck (*(data + 1));
+	int lightState = lightsCheck (*(data + 2));
 	
-	if (motorCheck(*data) && motorCheck(*data) == motorCheck(*(data + 1)) && lightsCheck(*(data + 2)) ) // set outputArray if motors data are valid, motors are in sync and lights data is valid
+	if (motorOneState && motorOneState == motorTwoState) // set outputArray values of motor if motors data are valid, motors are in sync
 	{
-		for (int i = 0; i < sizeof outputArray / sizeof *outputArray; i++)
-		{
-			outputArray[i] = *(data + i);
-		}	
+		outputArray[0] = *data;
+		outputArray[1] = *(data + 1);
 	}
 	else
 	{
-		for (int i = 0; i < sizeof outputArray / sizeof *outputArray; i++)
-		{
-			outputArray[i] = 0;
-		}	
+		outputArray[0] = 0;
+		outputArray[1] = 0;
 	}
 	
-	if (motorCheck (*data))  // set first motor bit in validData to 1 if motor's data is valid
+	if (lightState) // set outputArray values of lights if lights data are valid
+	{
+		outputArray[2] = *(data + 2);
+	}
+	else
+	{
+		outputArray[2] = 0;
+	}	
+	
+	if (motorOneState)  // set first motor bit in validData to 1 if motor's data is valid
 	{
 		validData = validData | (1 << FIRSTMOTOR);
 	}
 	
-	if (motorCheck (*(data + 1))) // set second motor bit in validData to 1 if motor's data is valid
+	if (motorTwoState) // set second motor bit in validData to 1 if motor's data is valid
 	{
 		validData = validData | (1 << SECONDMOTOR);
 	}
-	if (lightsCheck (*(data+2))) // set lights bit in validData to 1 if lights data is valid
+	
+	if (lightState) // set lights bit in validData to 1 if lights data is valid
 	{
 		validData = validData | (1 << LIGHTSBOARD);
 	}	
@@ -36,14 +45,17 @@ void trainingTask(uint8_t* data)
 
 int motorCheck (uint8_t testMotor)
 {
-	uint8_t motorMask = 0b11000001;
+	uint8_t forwardMask = 0b10000001;
+	uint8_t reverseMask = 0b11000000;
 	
-	if ((testMotor & motorMask) == 0b10000000) // check the case that motor is in reverse (power on, negative velocity and reverse)
+	if ((testMotor & forwardMask) == forwardMask) // check the case that motor is in reverse (power on, negative velocity and reverse)
 		return 1;
-	else if ((testMotor & motorMask) == motorMask) // check the case that motor is in forward (power on, positive velocity and forward)
+	else if ((testMotor & reverseMask) == reverseMask) // check the case that motor is in forward (power on, positive velocity and forward)
 		return 2;
-	else if (testMotor == 0) // check case that power is off
+	else if ((testMotor & 0b00000001) == 0b00000001) // check case that power is off and motor on "forward"
 		return 3;
+	else if (testMotor == 0) // check case that power is off
+		return 4;	
 	
 	return 0;
 }	
@@ -52,8 +64,12 @@ int lightsCheck (uint8_t testLight)
 {
 	uint8_t maskedHeadLights = testLight & 0b00000111;
 	uint8_t maskedSignals = testLight & 0b00011000;
+	uint8_t maskedHazards = testLight & 0b00111000;
+	uint8_t hazardsOn = testLight & 0b00100000;
+	int oneHeadlightOn = maskedHeadLights && !(maskedHeadLights & (maskedHeadLights-1));
+	int oneSignalOn = maskedSignals && !(maskedSignals & (maskedSignals-1));
 	
-	if(!(maskedHeadLights && !(maskedHeadLights & (maskedHeadLights-1))) || !(maskedSignals && !(maskedSignals & (maskedSignals-1)))) // if more than 1 headlight state is chosen or more than one signal is on return 0
+	if(!oneHeadlightOn || (hazardsOn && maskedHazards != 0b00111000) || (!hazardsOn && !oneSignalOn)) // if more than 1 headlight state is chosen or both signals are not on while hazard is on or more than one signal is on return 0
 		return 0;
 	
 	return 1;
--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -13,39 +13,38 @@ void runTrainingTests()
 void test_EverythingValid()
 {
 	printf("\nTesting trainingTask - Should Pass\n");
-	uint8_t inputData[3] = {0b11001011, 0b11010111, 0b00001010}; 
+	uint8_t inputData[3] = {0b10101011, 0b10101101, 0b00001010}; 
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY (inputData, outputArray, 3);
-	TEST_ASSERT_EQUAL_UINT8 (7, validData);
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (inputData, outputArray, 3, "All input data must be copied to outputArray as all data is valid");
+	TEST_ASSERT_EQUAL_UINT8_MESSAGE (7, validData, "validData should have the first 3 bits as 1 as all data inputs are valid");
 }
 
 void test_EverythingInvalid()
 {
 	printf("\nTesting trainingTask - Should Pass\n");
-	uint8_t inputData[3] = {0b01001011, 0b11010110, 0b01011101};
+	uint8_t inputData[3] = {0b10011010, 0b01011000, 0b00101110};
 	uint8_t expectedOutput[3] = {0};
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY (expectedOutput, outputArray, 3);
-	TEST_ASSERT_EQUAL_UINT8 (0, validData);
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "outputArray should have all zeros as none of the data inputs is valid");
+	TEST_ASSERT_EQUAL_UINT8_MESSAGE (0, validData, "The last 3 bits of validData should remain zero as all data is invalid");
 }
 
 void test_OnlyLightsInvalid()
 {
 	printf("\nTesting trainingTask - Should Pass\n");
-	uint8_t inputData[3] = {0b11001011, 0b11010111, 0b01011101};
-	uint8_t expectedOutput[3] = {0};
+	uint8_t inputData[3] = {0b10101011, 0b10101101, 0b00110010};
+	uint8_t expectedOutput[3] = {0b10101011, 0b10101101, 0};
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY (expectedOutput, outputArray, 3);
-	TEST_ASSERT_EQUAL_UINT8 (3, validData);
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "Only motors data should be copied into outputArray as these are valid data while the lights data is invlaid");
+	TEST_ASSERT_EQUAL_UINT8_MESSAGE (3, validData, "Bits 0 and 1 of validData should be set to 1 as they represent the motors which have valid inputs");
 }
 
 void test_OnlyMotorsInvalid()
 {
 	printf("\nTesting trainingTask - Should Pass\n");
-	uint8_t inputData[3] = {0b01001011, 0b11010110, 0b00001010};
-	uint8_t expectedOutput[3] = {0};
+	uint8_t inputData[3] = {0b10011010, 0b01011000, 0b00001010};
+	uint8_t expectedOutput[3] = {0, 0, 0b00001010};
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY (expectedOutput, outputArray, 3);
-	TEST_ASSERT_EQUAL_UINT8 (4, validData);
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "Only lights data should be copied into outputArray as these are valid data while the motors data is invlaid");
+	TEST_ASSERT_EQUAL_UINT8_MESSAGE (4, validData, "Bit 3 of validData should be set to 1 as it represents lights data which is valid");
 }
-
