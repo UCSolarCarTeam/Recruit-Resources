4e128e9ac755d65eb97f285542adc96d9092b5ce
Rahat Islam <islamrahat735@gmail.com>
2020-11-21 11:39:18 -0700

    modifcations to the tests

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -16,7 +16,12 @@ void test_EverythingValid()
 {
     uint8_t input[3] = {0b11111110, 0b11111110, 0b01111100};
     uint8_t expectedValidData = 0b111;
-    uint8_t expectedOutput[3] = {0b11111110, 0b11111110, 0b01111100};
+    uint8_t expectedOutput[3] = {input[0], input[1], input[2]};
+
+    previousOutput[0] = input[0];
+    previousOutput[1] = input[1];
+    previousOutput[2] = input[2];
+
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
@@ -27,17 +32,22 @@ void test_EverythingInvalid()
 {
     uint8_t input[3] = {0b00111110, 0b01111110, 0b01110101};
     uint8_t expectedValidData = 0b000;
-    uint8_t expectedOutput[3] = {0b11111110, 0b11111110, 0b01111100};
+    uint8_t expectedOutput[3] = {previousOutput[0], previousOutput[1], previousOutput[2]};
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
+
 }
 
 void test_OnlyLightsInvalid()
 {
-    uint8_t input[3] = {0b10000000, 0b10000000, 0b01011100};
+    uint8_t input[3] = {0b11000000, 0b11000000, 0b01011100};
     uint8_t expectedValidData = 0b011;
-    uint8_t expectedOutput[3] = {0b10000000, 0b10000000, 0b01111100};
+    uint8_t expectedOutput[3] = {input[0], input[1], previousOutput[2]};
+
+    previousOutput[0] = input[0];
+    previousOutput[1] = input[1];
+
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
@@ -47,9 +57,12 @@ void test_OnlyMotorsInvalid()
 {
     uint8_t input[3] = {0b00111110, 0b00111110, 0b01010100};
     uint8_t expectedValidData = 0b100;
-    uint8_t expectedOutput[3] = {0b10000000, 0b10000000, 0b01010100};
+    uint8_t expectedOutput[3] = {previousOutput[0], previousOutput[1], input[2]};
+
+    previousOutput[2] = input[2];
+
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
-
 }
+
--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.h
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.h
@@ -5,3 +5,5 @@ void test_EverythingValid();
 void test_EverythingInvalid();
 void test_OnlyLightsInvalid();
 void test_OnlyMotorsInvalid();
+
+uint8_t previousOutput[3];
