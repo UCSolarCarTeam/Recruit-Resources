2dc3fdda71e8979a8c385e351b53f94624392662
Rahat Islam <islamrahat735@gmail.com>
2020-11-20 21:32:15 -0700

    Changed up the code to make it cleaner. But honestly, it might just be even more complicated *facepalm*

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -3,58 +3,134 @@
     CONDITIONS FOR FAILURE
     
     -the velocity and direction bit don't express the same direction
-    -the input for the first and second motor aren't EXACTLY the same
+    -If the motors are off, and the speeds dont have the same direction
+    -Motors dont have the same direction
+    -Motors aren't both on or both off
     -more than one headlight choice is chosen
-    -both left and right signals are on at the same time
+    -both left and right signals are on at the same time when the hazards are off
+    -hazards are on, but the signals are off
+
+    **NOTE** - 2's compliment system is being used for velocity
 
 */
 void trainingTask(uint8_t* data)
 {
-    uint8_t tempVelocityMotor1 = data[0] >> 6;
-    tempVelocityMotor1 = tempVelocityMotor1 << 7;
+    //MOTORS
 
-    uint8_t tempVelocityMotor2 = data[0] >> 6;
-    tempVelocityMotor2 = tempVelocityMotor2 << 7;
+    uint8_t motor1Direction = data[0] & 1;
+    uint8_t motor2Direction = data[1] & 1;
 
-    if ((data[0] <<7 != tempVelocityMotor1)
-         && (data[1] << 7 != tempVelocityMotor2))
+    uint8_t motor1Velocity = data[0] >> 1;
+    motor1Velocity &= 0b00111111;
+
+    uint8_t motor2Velocity = data[1] >> 1;
+    motor2Velocity &= 0b00111111;
+    
+    if ((data[0] >> 7) != (data[1] >> 7)) // if the motors are not both ON or both OFF
     {
-        if (data[0] == data[1])
-        {
-            outputArray[0] = data[0];
-            outputArray[1] = data[1];
-            validData |= 0b0000011;
-        }
-        else 
-        {
-           validData &= 0b11111100;  
-        }
-    }
-    else 
-    {
-        validData &= 0b11111100;    
+        invalidMotorInput();
     }
     
-    //Lights
-    if (   (data[2]&0b00000111) == 0b100 
-        || (data[2]&0b00000111) == 0b010
-        || (data[2]&0b00000111) == 0b001)
+    else if (   ((data[0] >> 7) == 0)  // if  motor 1 is off AND the speed is not 0
+             && (motor1Velocity != 0)
+            )
+    {
+        invalidMotorInput();
+    }
+
+    else if (   ((data[1] >> 7) == 0)  // if  motor 2 is off AND the speed isn't 0
+             && (motor2Velocity != 0)
+            )
+    {
+        invalidMotorInput();   
+    }
+   
+    else if (      (motor1Velocity != 0)                        //if motor1 velocity is not 0
+                && (((data[0] >> 6) & 1) == motor1Direction)      //AND velocity sign bit and direction bit are the same 
+            ) 
         {
-            if ( (data[2] & 0b00011000) != 0b00011000)
-            {
-                outputArray[2] = data[2];
-                validData |= 0b00000100;
-            }
-            else
-            {
-                validData &= 0b11111011;
-            }
-            
+            invalidMotorInput();
         }
+    
+
+    else if (      (motor2Velocity != 0)                        //if motor2 velocity is not 0
+                && (((data[1] >> 6) & 1) == motor2Direction)      //AND velocity sign bit and direction bit are the same 
+            ) 
+        {
+            invalidMotorInput();
+        }
+    
+    
+    else if (motor1Direction != motor2Direction)           //       if motor 1 and motor 2 have opposite direction 
+    {
+        invalidMotorInput();
+    }
 
     else
     {
-        validData &= 0b11111011;
+        validMotorInput(data);
+    }
+       
+    //LIGHTS
+
+    uint8_t rightSignal = (data[2] >> 3) & 1;
+    uint8_t leftSignal = (data[2] >> 4) & 1;
+    
+    if (   (data[2]&0b00000111) != 0b100 
+        && (data[2]&0b00000111) != 0b010
+        && (data[2]&0b00000111) != 0b001)  // if more than one headlight is active
+        {
+            invalidLightsInput();            
+        }
+    
+    else if (     ((data[2] >> 5) & 1) == 1         //if    hazards lights are ON
+                &&(leftSignal + rightSignal != 2)   //      AND the left and right signals are not Both ON
+            )
+        {
+            invalidLightsInput();
+        }
+
+    else if (     ((data[2] >> 5) & 1) == 0         //if    hazards lights are OFF
+                &&(leftSignal + rightSignal == 2)   //      AND the left and right signals are Both ON
+            )
+        {
+            invalidLightsInput();
+        }
+    
+    else
+    {
+        validLightsInput(data);
     }
     
+}
+
+//ADDED FUNCTIONS
+
+void invalidMotorInput()
+{
+    validData &= 0b11111100;
+    return;
+}
+
+void invalidLightsInput()
+{
+    validData &= 0b11111011;
+    return;
+}
+
+void validMotorInput(uint8_t* data)
+{
+    outputArray[0] = data[0];
+    outputArray[1] = data[1];
+    validData |= 0b0000011;
+
+    return;
+}
+
+void validLightsInput(uint8_t* data)
+{
+    outputArray[2] = data[2];
+    validData |= 0b00000100;
+
+    return;
 }
--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -14,32 +14,42 @@ void runTrainingTests()
 
 void test_EverythingValid()
 {
-    uint8_t input[3] = {0b00111110, 0b00111110, 0b01110100};
+    uint8_t input[3] = {0b11111110, 0b11111110, 0b01111100};
     uint8_t expectedValidData = 0b111;
+    uint8_t expectedOutput[3] = {0b11111110, 0b11111110, 0b01111100};
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
+
 }
 
 void test_EverythingInvalid()
 {
     uint8_t input[3] = {0b00111110, 0b01111110, 0b01110101};
     uint8_t expectedValidData = 0b000;
+    uint8_t expectedOutput[3] = {0b11111110, 0b11111110, 0b01111100};
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
 }
 
 void test_OnlyLightsInvalid()
 {
-uint8_t input[3] = {0b00111110, 0b00111110, 0b01111100};
+    uint8_t input[3] = {0b10000000, 0b10000000, 0b01011100};
     uint8_t expectedValidData = 0b011;
+    uint8_t expectedOutput[3] = {0b10000000, 0b10000000, 0b01111100};
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
 }
 
 void test_OnlyMotorsInvalid()
 {
-uint8_t input[3] = {0b00111111, 0b00111110, 0b01110100};
+    uint8_t input[3] = {0b00111110, 0b00111110, 0b01010100};
     uint8_t expectedValidData = 0b100;
+    uint8_t expectedOutput[3] = {0b10000000, 0b10000000, 0b01010100};
     trainingTask(input);
     TEST_ASSERT_EQUAL_MESSAGE(expectedValidData,validData, "validData and its expected value doesnt match");
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedOutput, outputArray, 3, "Arrays don't match");
+
 }
