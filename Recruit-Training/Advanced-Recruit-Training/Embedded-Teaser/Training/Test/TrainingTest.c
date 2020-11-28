5eede295ebab237858aa77bae1bc1e51d7d773ad
Ahmed_Abdullah <ahmedi3aaa@hotmail.com>
2020-11-28 20:08:18 +0200

    fixed spelling mistakes in messages :D

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -14,7 +14,7 @@ void test_EverythingValid()
 {
 	uint8_t inputData[3] = {0b10101011, 0b10101101, 0b00001010}; 
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (inputData, outputArray, 3, "One or more datainput wasn't copied to oytputArray");
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (inputData, outputArray, 3, "One or more data input wasn't copied to outputArray");
 	TEST_ASSERT_EQUAL_UINT8_MESSAGE (7, validData, "Not all 3 bit of validData are 1");
 }
 
@@ -23,7 +23,7 @@ void test_EverythingInvalid()
 	uint8_t inputData[3] = {0b10011010, 0b01011000, 0b00101110};
 	uint8_t expectedOutput[3] = {0};
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "oytputArray is not all zeros");
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "outputArray is not all zeros");
 	TEST_ASSERT_EQUAL_UINT8_MESSAGE (0, validData, "last 3 bits of validData are not zero");
 }
 
@@ -32,7 +32,7 @@ void test_OnlyLightsInvalid()
 	uint8_t inputData[3] = {0b10101011, 0b10101101, 0b00110010};
 	uint8_t expectedOutput[3] = {0b10101011, 0b10101101, 0};
 	trainingTask (inputData);
-	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "motors data should be copied to outputArray but not lights data");
+	TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE (expectedOutput, outputArray, 3, "only motors data should be copied to outputArray");
 	TEST_ASSERT_EQUAL_UINT8_MESSAGE (3, validData, "Bits 0 and 1 of validData should be set to 1");
 }
 
