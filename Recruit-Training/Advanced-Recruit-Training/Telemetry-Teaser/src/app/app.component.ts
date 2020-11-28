0822da7eb732352cb10d74b1dee46c9ec3bbf244
Dillonsd <dillonapple7h@gmail.com>
2020-11-29 00:31:56 +0400

    Telemetry Teaser

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   public input: any;
   title = 'Telemetry Recruit Training';
   weather = 50;
-  speed = 0;
-  battery = 0;
+  speed:number = 0;
+  battery:number = 0;
+  range:number = 0;
+  displayText:string = "";
+  speedError:string = "";
+  batteryError:string = "";
+  speedInput:FormControl;
+  batteryInput:FormControl;
+  rangeForm:FormGroup
+
+  ngOnInit(): void {
+    this.speedInput = new FormControl('', [ Validators.required,
+                                            Validators.min(0),
+                                            Validators.max(90)]);
+    this.batteryInput = new FormControl('', [ Validators.required,
+                                              Validators.min(0),
+                                              Validators.max(100)]);
+
+    this.rangeForm = new FormGroup({
+      'speedInput': this.speedInput,
+      'batteryInput': this.batteryInput
+    });
+  }
 
   onMouseWeather(event: MouseEvent) {
     this.weather = parseInt((event.target as HTMLInputElement).value);
@@ -27,6 +49,9 @@ export class AppComponent {
   }
 
   calculateRange() {
-
+    this.range = -(this.speed * this.speed * this.battery / 2500) +
+      (4 * this.battery) + this.weather;
+    this.displayText = "The predicted range of the Eylsia is " + 
+    this.range + " km.";
   }
 }
