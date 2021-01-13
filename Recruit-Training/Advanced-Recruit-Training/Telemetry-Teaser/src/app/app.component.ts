0fdaee51ab9da5f55b05a3aebd57933e51c7912a
Bryce <brycepcayanan@gmail.com>
2021-01-12 17:11:27 -0700

    Finished telemetry

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
@@ -11,8 +11,11 @@ export class AppComponent {
   public input: any;
   title = 'Telemetry Recruit Training';
   weather = 50;
-  speed = 0;
-  battery = 0;
+  speed = NaN;
+  battery = NaN;
+  range = 0;
+  batteryError = 0;
+  speedError = 0;
 
   onMouseWeather(event: MouseEvent) {
     this.weather = parseInt((event.target as HTMLInputElement).value);
@@ -26,7 +29,37 @@ export class AppComponent {
     this.battery = parseInt((event.target as HTMLInputElement).value);
   }
 
-  calculateRange() {
+  calculateRange(event: MouseEvent) {
+    if(this.validate()){
+      this.range = -(this.speed * this.speed * this.battery / 2500) + (4 * this.battery) + this.weather;
+    }
+  }
 
+  validate(){
+    if(isNaN(this.speed)){
+      this.speedError = 1;
+    } 
+    else if(this.speed < 0 || 90 < this.speed){
+      this.speedError = 2;
+    }
+    else {
+      this.speedError = 0;
+    }
+
+    if(isNaN(this.battery)){
+      this.batteryError = 1;
+    } 
+    else if(this.battery < 0 || 100 < this.battery){
+      this.batteryError = 2;
+    }
+    else {
+      this.batteryError = 0;
+    }
+
+    if((this.batteryError + this.speedError) != 0){
+      return false;
+    }
+
+    return true;
   }
 }
