5eafea7e3bd103bf55fcb027d267e23420df6be6
MareceloLiGonzales <marcelo.ligonzales@ucalgary.ca>
2021-01-14 19:16:56 -0700

    Fixed stylistic conventions

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Inc/Training.h
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Inc/Training.h
@@ -6,10 +6,10 @@ uint8_t validData; // Set bits 0, 1, 2 if the input array elements were valid
 
 void trainingTask(uint8_t* data); // data should be 3 elements wide (size of 3)
 
-uint8_t getbit(uint8_t data,int number);
+uint8_t getBit(uint8_t data,int number);
 
-int getvelocity(uint8_t data);
+int getVelocity(uint8_t data);
 
-void checkmotors(uint8_t *validity, uint8_t motor1, uint8_t motor2);
+void checkMotors(uint8_t *validity, uint8_t motor1, uint8_t motor2);
 
-void checklights(uint8_t *validity, uint8_t lights);
+void checkLights(uint8_t *validity, uint8_t lights);
--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -5,16 +5,16 @@ void trainingTask(uint8_t* data)
 {
     uint8_t validity = 0b00000111;
 
-    checkmotors(&validity, data[0], data[1]);
+    checkMotors(&validity, data[0], data[1]);
 
-    checklights(&validity, data[2]);
+    checkLights(&validity, data[2]);
 
 
-    if( getbit(validity, 0) && getbit(validity, 1)) {
+    if (getBit(validity, 0) && getBit(validity, 1)) {
         outputArray[0] = data[0];
         outputArray[1] = data[1];
     }
-    if (getbit(validity, 2)){
+    if (getBit(validity, 2)){
         outputArray[2] = data[2];
     }
 
@@ -22,34 +22,34 @@ void trainingTask(uint8_t* data)
 
 }
 
-uint8_t getbit(uint8_t data,int number)
+uint8_t getBit(uint8_t data,int number)
 {
     return (data & 1<<number);
 }
 
-int getvelocity(uint8_t data)
+int getVelocity(uint8_t data)
 {
-    int VelocityBits  = getbit(data, 1) + getbit(data, 2) + getbit(data, 3) + getbit(data, 4) + getbit(data, 5);
-    int velocity = VelocityBits >> 1;
+    int velocityBits  = getBit(data, 1) + getBit(data, 2) + getBit(data, 3) + getBit(data, 4) + getBit(data, 5);
+    int velocity = velocityBits >> 1;
 
-    if (getbit(data, 6))
+    if (getBit(data, 6))
     velocity *=-1;
 
     return velocity;
 }
 
-void checkmotors(uint8_t *validity, uint8_t motor1, uint8_t motor2)
+void checkMotors(uint8_t *validity, uint8_t motor1, uint8_t motor2)
 {
-    int velocity1 = getvelocity(motor1);
-    int velocity2 = getvelocity(motor2);
+    int velocity1 = getVelocity(motor1);
+    int velocity2 = getVelocity(motor2);
 
-    int Motor1State = getbit(motor1, 7);
-    if (Motor1State != getbit(motor2, 7)){  //check both motors are either turned on or off
+    int motor1State = getBit(motor1, 7);
+    if (motor1State != getBit(motor2, 7)) {  //check both motors are either turned on or off
         *validity &= ~0b00000011;
         return;
     } 
 
-    if(Motor1State == 0){ //if motors are off, check velocity is 0
+    if (motor1State == 0) { //if motors are off, check velocity is 0
 
         if (velocity1 != 0)
         *validity &= ~0b00000011;
@@ -61,35 +61,35 @@ void checkmotors(uint8_t *validity, uint8_t motor1, uint8_t motor2)
         if (velocity1*velocity2 < 0)  //check velocities are going the same direction
         *validity &= ~0b00000011;
 
-        int VelocityDirection1 = velocity1 > 0;
-        if (getbit(motor1, 0) != VelocityDirection1) // check motor 1 direction matches its velocity
+        int velocityDirection1 = velocity1 > 0;
+        if (getBit(motor1, 0) != velocityDirection1) // check motor 1 direction matches its velocity
         *validity &= ~0b00000011;
 
-        int VelocityDirection2 = velocity2 > 0;
-        if (getbit(motor2, 0) != VelocityDirection2) // check motor 2 direction matches its velocity
+        int velocityDirection2 = velocity2 > 0;
+        if (getBit(motor2, 0) != velocityDirection2) // check motor 2 direction matches its velocity
         *validity &= ~0b00000011;
     }
 
 }
 
-void checklights(uint8_t *validity, uint8_t lights)
+void checkLights(uint8_t *validity, uint8_t lights)
 {
 
-    if (!getbit(lights, 5)) {                        
-         if (getbit(lights, 3) && getbit(lights, 4))     //if hazards lights are not on, check that not both turn signals are on
+    if (!getBit(lights, 5)) {                        
+         if (getBit(lights, 3) && getBit(lights, 4))     //if hazards lights are not on, check that not both turn signals are on
         *validity &= ~0b00000100;
     }                       
 
-    int Lowlights = getbit(lights, 1);
-    int Highlights = getbit(lights, 2);
+    int lowlights = getBit(lights, 1);
+    int highlights = getBit(lights, 2);
 
-    if (getbit(lights, 0)) {                        //if headlights are off, check all modes are off
-        if (Lowlights || Highlights)
+    if (getBit(lights, 0)) {                        //if headlights are off, check all modes are off
+        if (lowlights || highlights)
         *validity &= ~0b00000100;
     } else {                                        // if headlights are on, check that exactly one mode is on
-        if (!Lowlights && !Highlights)
+        if (!lowlights && !highlights)
         *validity &= ~0b00000100;
-        if (Lowlights && Highlights)               
+        if (lowlights && highlights)               
         *validity &= ~0b00000100;
     }
 }
