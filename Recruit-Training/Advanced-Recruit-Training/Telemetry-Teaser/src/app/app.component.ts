9df422c7c9ba236350fc4ca2a0b1fbdb94b127f0
MarceloLiGonzales <marcelo.ligonzales@ucalgary.ca>
2021-01-21 19:19:32 -0700

    Implemented changes requested by jayden

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent {
   weather = 50;
   speed = 0;
   battery = 0;
-  Sinput = new FormControl(0, [Validators.required, Validators.min(0), Validators.max(90)]);
-  Binput = new FormControl(0, [Validators.required, Validators.min(0), Validators.max(100)]);
+  speedInput = new FormControl(null, [Validators.required, Validators.min(0), Validators.max(90)]);
+  batteryInput = new FormControl(null, [Validators.required, Validators.min(0), Validators.max(100)]);
 
   onMouseWeather(event: MouseEvent) {
     this.weather = parseInt((event.target as HTMLInputElement).value);
@@ -31,8 +31,8 @@ export class AppComponent {
 
 
   calculateRange() {
-    this.speed = this.Sinput.value
-    this.battery = this.Binput.value
+    this.speed = this.speedInput.value;
+    this.battery = this.batteryInput.value;
     this.weather = parseInt((document.getElementById("weather") as HTMLInputElement).value);
     var range = -1 * (this.speed * this.speed * this.battery / 2500) + (4 * this.battery) + this.weather;
     document.getElementById("Range").innerHTML = "The predicted range of the Elysisa is " + String(range) + " km.";
